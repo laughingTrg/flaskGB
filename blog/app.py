5b6c0b2546da6b44ecfa8a49fcbafce7a6bacb99
laughing <laughinChrist@gmail.com>
2023-05-07 18:04:20 +0500

    lesson_3 ready

--- blog/app.py
+++ blog/app.py
@@ -1,15 +1,20 @@
+from os import getenv, path
 from flask import Flask
+from json import load
+#from .models import User
 
 from blog.user import views as user_views
 from blog.article import views as article_views
 from blog.index import views as index_views
+from blog.auth import views as auth_views
+from blog.extenshion import db, login_manager
 
-#@app.route('/')
-#def index():
-#    return "Hello a new web blog!"
+CONFIG_PATH = getenv("CONFIG_PATH", path.join("../config.json"))
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    app.config.from_file(CONFIG_PATH, load)
+    register_extenshions(app)
     register_blueprints(app)
     return app
     
@@ -17,4 +22,16 @@ def register_blueprints(app: Flask):
     app.register_blueprint(user_views.user)
     app.register_blueprint(article_views.article)
     app.register_blueprint(index_views.index)
+    app.register_blueprint(auth_views.auth)
 
+
+def register_extenshions(app: Flask):
+    db.init_app(app)
+
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        from .models import User
+        return User.query.get(int(user_id))
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
+from werkzeug.security import generate_password_hash
+from blog.app import create_app, db
 
-from blog.app import create_app
-
+app = create_app()
 
 if __name__ == '__main__':
     app = create_app()
@@ -8,3 +9,16 @@ if __name__ == '__main__':
         host='0.0.0.0',
         debug=True,
             )
+
+@app.cli.command('init-db', help='create all db')
+def init_db():
+    from blog.models import User
+    db.create_all()
+
+@app.cli.command('create-users', help='create-users')
+def create_users():
+    from blog.models import User
+    db.session.add(
+        User(email="some@email.com", password=generate_password_hash("test"))
+            )
+    db.session.commit()
