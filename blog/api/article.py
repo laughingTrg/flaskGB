f174d39f61ddf7d259dd6ad985fa8cd3b0cea013
laughing <laughinChrist@gmail.com>
2023-06-02 06:56:21 +0500

    create api permissions and rpc

--- blog/api/article.py
+++ blog/api/article.py
@@ -1,10 +1,16 @@
 from flask_combo_jsonapi import ResourceDetail, ResourceList
+from combojsonapi.event.resource import EventsResource
 
 from blog.schemas import ArticleSchema
 from blog.models import Article
 from blog.extenshion import db
 
+class ArticleListEvents(EventsResource):
+    def event_get_count(self):
+        return {'count': Article.query.count()}
+
 class ArticleList(ResourceList):
+    events = ArticleListEvents
     schema = ArticleSchema
     data_layer = {
             'session': db.session,
--- blog/api/user.py
+++ blog/api/user.py
@@ -1,4 +1,5 @@
 from flask_combo_jsonapi import ResourceDetail, ResourceList
+from blog.permissions.user import UserPermission
 
 from blog.schemas import UserSchema
 from blog.models import User
@@ -16,6 +17,7 @@ class UserDetail(ResourceDetail):
     data_layer = {
             'session': db.session,
             'model': User,
+            'permission_get': [UserPermission],
             }
 
 
